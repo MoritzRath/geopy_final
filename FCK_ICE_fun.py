b67ac7d4cae7feacdb149cb53660cf8e62d16a0a
MoritzRath <moritzrath@gmx.de>
2026-02-23 11:49:41 +0100

    create coregister function and test

--- FCK_ICE_fun.py
+++ FCK_ICE_fun.py
@@ -1,2 +1,62 @@
 # In this file I gather functions, that I'll use in the processing.
+import os
+import ee
+import geemap
+import tempfile
+from arosics import COREG_LOCAL
+from pathlib import Path
+
+def getS1date(img):
+    index = img.get('system:index').getInfo()
+    date = index.split('_')[4][:8]
+    file = f"{date}.tif"
+    return file
+
+
+def coregisterS1(s1, region, kwargs):
+    """
+    This function coregisters two consecutive Sentinel-1 iteratively images from an GEE ImageCollection.
+    The function then stores the coregistered images in a given directory.
+    Following variables:
+
+    s1:             Image collection of S1-Scenes obtained via GEE
+    region:         regional extent in which the coregistration will be performed
+    kwargs:         parameters for coregistering
+    """
+
+    # fix regional extent
+    regionEXP = ee.FeatureCollection(region).geometry()
+    regionEXP = regionEXP.transform('EPSG:4326', maxError=1)
+
+    # Transform S1-Scenes to list
+    collection = s1.toList(s1.size())
+    n_images = s1.size().getInfo()
+
+    # Initiate temp-directory
+    temp_dir = tempfile.mkdtemp()
+
+    for i in range(n_images-1):
+        img1 = ee.Image(collection.get(i))
+        img2 = ee.Image(collection.get(i+1))
+
+        file1 = getS1date(img1)
+        file2 = getS1date(img2)
+
+        path_img1 = os.path.join(temp_dir, file1)
+        path_img2 = os.path.join(temp_dir, file2)
+
+        geemap.ee_export_image(img1, filename=path_img1, scale=10,
+                               region=regionEXP, file_per_band=False)
+        geemap.ee_export_image(img2, filename=path_img2, scale=10,
+                               region=regionEXP, file_per_band=False)
+        
+        coreg = COREG_LOCAL(path_img2, path_img1, **kwargs)
+        coreg.correct_shifts()
+        crl_afterCORR = COREG_LOCAL(path_img2, coreg.path_out, **kwargs)
+        crl_afterCORR.view_CoRegPoints(figsize=(15,15), backgroundIm='ref')
+        coreg = None
+        crl_afterCORR = None
+        
+
+
 
